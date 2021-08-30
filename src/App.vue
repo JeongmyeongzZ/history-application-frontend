c561c9bf21c966c67b68411ac41c0ecead05d729
jeongmyeong <kimbym23@gmail.com>
2021-08-30 21:58:03 +0900

    Header component 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <main>
+    <global-header title="kurly history" />
   </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import GlobalHeader from '@/components/Header.vue';
 
 @Options({
   components: {
+    GlobalHeader,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+})
+export default class Header extends Vue {}
+</script>
+
+<style scoped lang="scss">
+  header {
+    padding: 1em 1.5em;
+
+    h1 {
+      font-family: 'Alegreya Sans SC', sans-serif;
+      font-size: 1rem;
+      text-transform: uppercase;
+      letter-spacing: 0.125rem;
+    }
+  }
+</style>
