6a5cd9a3c43a1a6f7f4f2a4a711b92c5c5ae551b
jeongmyeong <kimbym23@gmail.com>
2021-09-13 21:45:31 +0900

    vue-image-kit import and use

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './app.css';
 
-createApp(App).mount('#app');
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line import/order
+import VueImageKit from 'vue-image-kit';
+
+const app = createApp(App);
+
+app.use(VueImageKit);
+app.mount('#app');
